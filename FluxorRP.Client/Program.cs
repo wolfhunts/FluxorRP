7a9c3b3589c3cd0bd598eca6a39ee389f7d453e4
Cedric ZULIANI <zulianiCedric@hotmail.com>
2024-11-26 15:50:57 +0100

    resolution bug interference

--- FluxorRP.Client/Program.cs
+++ FluxorRP.Client/Program.cs
@@ -1,14 +1,23 @@
 using Fluxor;
 using Fluxor.Blazor.Web.ReduxDevTools;
+using FluxorRP.Shared.Data;
+using FluxorRP.Shared.Store.ListCharacter;
+using FluxorRP.Shared.Store.ListMonster;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.dejete.com") });
-
+builder.Services.AddDbContext<FluxorRPContext>(options =>
+    options.UseSqlite($"Data Source={Path.Combine(Directory.GetCurrentDirectory(), "FluxorRP.db")}"));
+builder.Services.AddScoped<ListCharacterFeature>();
+builder.Services.AddScoped<ListMonsterFeature>();
 builder.Services.AddFluxor(options =>
 {
-    options.ScanAssemblies(typeof(Program).Assembly, typeof(FluxorRP.Shared.Store.Dice.DiceRollAction).Assembly);
+    options.ScanAssemblies(typeof(Program).Assembly, typeof(FluxorRP.Shared.Store.Dice.DiceRollAction).Assembly,
+    typeof(FluxorRP.Shared.Store.ListCharacter.ListCharacterSelectAction).Assembly,
+                    typeof(FluxorRP.Shared.Store.ListMonster.ListMonsterSelectAction).Assembly);
 
     options.UseReduxDevTools();
 });
