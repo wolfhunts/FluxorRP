db2e9f80108058b7afa69e497ffe08c24533b167
Cedric ZULIANI <zulianiCedric@hotmail.com>
2024-11-27 14:17:13 +0100

    Revert "fix"
    This reverts commit 6b41e6081f6c2630d2bf68b22b19d0277016c173.

--- FluxorRP.Shared/Store/Monster/MonsterEffect.cs
+++ FluxorRP.Shared/Store/Monster/MonsterEffect.cs
@@ -18,9 +18,8 @@ namespace FluxorRP.Shared.Store.Monster
         public IState<CharacterState> CharacterState { get; }
 
         [EffectMethod(typeof(MonsterLaunchAttackAction))]
-        public Task MonsterAttackEffect(IDispatcher dispatcher)
+        public Task AddHealthFromDice(IDispatcher dispatcher)
         {            
-            dispatcher.Dispatch(new CharacterAddHealthAction(0 - new Random().Next(1, this.CharacterState.Value.strength)));
             dispatcher.Dispatch(new MonsterAttackAction(this.CharacterState.Value.strength));
 
             return Task.CompletedTask;
